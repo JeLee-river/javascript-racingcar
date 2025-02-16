2b0306ca4ef67ab5ad1eb975a5acc75461433d7e
Jeongeun Lee <leeju1009@gmail.com>
2025-02-16 21:08:27 +0900

    refactor(Utils.test): 테스트의 설명을 구체적으로 수정

--- __tests__/Utils.test.js
+++ __tests__/Utils.test.js
@@ -8,7 +8,7 @@ describe('랜덤으로 특정 범위의 정수를 반환하는 chooseRandomNumbe
       { minNumber: 9, maxNumber: 9 },
     ],
   ])(
-    'chooseRandomNumber 함수는 인자로 받은 범위 내의 숫자만 반환한다.',
+    '함수 인자로 최솟값과 최댓값을 받아, 해당 범위 내의 숫자만 반환한다.',
     ({ minNumber, maxNumber }) => {
       const randomNumber = chooseRandomNumber(minNumber, maxNumber);
 
@@ -17,7 +17,7 @@ describe('랜덤으로 특정 범위의 정수를 반환하는 chooseRandomNumbe
     },
   );
 
-  test('chooseRandomNumber 함수는 정수인 숫자만 반환한다.', () => {
+  test('0 이상 9 이하의 정수인 숫자만 반환한다.', () => {
     const MIN_NUMBER = 0;
     const MAX_NUMBER = 9;
 
